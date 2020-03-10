9fdc88df1d779c345ad7a0cc6b374f13981fdc29
Sudipto Bhattacharjee <ttsudipto@gmail.com>
2020-03-11 02:13:38 +0530

    Basic implementation of classification by processing inputs from PHP

--- python/driver.py
+++ python/driver.py
@@ -23,3 +23,24 @@ def parse_json(j_string) :
 inp = parse_json(sys.argv[1])
 for i in range(inp.param_length) :
     print(inp.get_param(i) + ' = ' + str(inp.get_value(i)))
+
+models = load_saved_models('SVM')
+score_sum = 0
+threshold_sum = 0
+for m in models :
+    threshold_sum = threshold_sum + m.optimal_threshold
+    model_score_sum = 0
+    for i in range(m.n_folds) :
+        y_pred = m.get_decision_score(m.estimators[i], inp.get_ndarray())
+        model_score_sum = model_score_sum + y_pred[0]
+    score_sum = score_sum + (model_score_sum / float(m.n_folds))
+    print('Model score : ' + str((model_score_sum / float(m.n_folds))))
+    print('Model threshold : ' + str(m.optimal_threshold))
+avg_threshold = threshold_sum / float(len(models))
+avg_score = score_sum / float(len(models))
+print('Mean Score : ' + str(avg_score))
+print('Mean Threshold : ' + str(avg_threshold))
+if avg_score < avg_threshold :
+    print('Predicted class : 0')
+else :
+    print('Predicted class : 1')
