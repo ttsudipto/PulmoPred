73e1fe9c857318c331700848b920a2f6526ef2f0
Sudipto Bhattacharjee <ttsudipto@gmail.com>
2020-03-16 18:39:32 +0530

    Display positiveness and negativeness of SVM score

--- index.php
+++ index.php
@@ -10,6 +10,26 @@
         <script type = "text/javascript" src = "js/plot.js"></script>
     </head>
     <body>
+        <style>
+            * {
+                box-sizing: border-box;
+            }
+            .plot_container::after {
+                content: "";
+                clear: both;
+                display: table;
+            }
+            .plot_div {
+                width: 100%;
+                height: 300px;
+            }
+            .plot_caption {
+                width: 100%;
+                text-align: center;
+                background-color: #cce6ff;
+                font-weight: bold;
+            }
+        </style>
         <div class = "section_header">
             <center> <h1> Heading </h1> </center>
         </div>
@@ -98,69 +118,92 @@
                     <th>Predicted score</th>
                     <th>Threshold</th>
                     <th>Predicted class</th>
+                    <th>Positiveness (%)</th>
+                    <th>Negativeness (%)</th>
                 </tr>
                 <tr>
                     <td><b>Classifier 0</b></td>
-                    <td><?php echo round(floatval($result->score0), 2); ?></td>
+                    <td><?php echo round(floatval($result->score0), 3); ?></td>
                     <td><?php echo $result->threshold0; ?></td>
                     <td><?php echo $result->predicted_label0; ?></td>
+                    <td><?php echo round(floatval($result->positiveness0), 4); ?></td>
+                    <td><?php echo round(floatval($result->negativeness0), 4); ?></td>
                 </tr>
                 <tr>
                     <td><b>Classifier 1</b></td>
-                    <td><?php echo round(floatval($result->score1), 2); ?></td>
+                    <td><?php echo round(floatval($result->score1), 3); ?></td>
                     <td><?php echo $result->threshold1; ?></td>
                     <td><?php echo $result->predicted_label1; ?></td>
+                    <td><?php echo round(floatval($result->positiveness1), 4); ?></td>
+                    <td><?php echo round(floatval($result->negativeness1), 4); ?></td>
                 </tr>
                 <tr>
                     <td><b>Classifier 2</b></td>
-                    <td><?php echo round(floatval($result->score2), 2); ?></td>
+                    <td><?php echo round(floatval($result->score2), 3); ?></td>
                     <td><?php echo $result->threshold2; ?></td>
                     <td><?php echo $result->predicted_label2; ?></td>
+                    <td><?php echo round(floatval($result->positiveness2), 4); ?></td>
+                    <td><?php echo round(floatval($result->negativeness2), 4); ?></td>
                 </tr>
                 <tr>
                     <td><b>Classifier 3</b></td>
-                    <td><?php echo round(floatval($result->score3), 2); ?></td>
+                    <td><?php echo round(floatval($result->score3), 3); ?></td>
                     <td><?php echo $result->threshold3; ?></td>
                     <td><?php echo $result->predicted_label3; ?></td>
+                    <td><?php echo round(floatval($result->positiveness3), 4); ?></td>
+                    <td><?php echo round(floatval($result->negativeness3), 4); ?></td>
                 </tr>
                 <tr>
                     <td><b>Classifier 4</b></td>
-                    <td><?php echo round(floatval($result->score4), 2); ?></td>
+                    <td><?php echo round(floatval($result->score4), 3); ?></td>
                     <td><?php echo $result->threshold4; ?></td>
                     <td><?php echo $result->predicted_label4; ?></td>
+                    <td><?php echo round(floatval($result->positiveness4), 4); ?></td>
+                    <td><?php echo round(floatval($result->negativeness4), 4); ?></td>
                 </tr>
                 <tr>
                     <td><b>Classifier 5</b></td>
-                    <td><?php echo round(floatval($result->score5), 2); ?></td>
+                    <td><?php echo round(floatval($result->score5), 3); ?></td>
                     <td><?php echo $result->threshold5; ?></td>
                     <td><?php echo $result->predicted_label5; ?></td>
+                    <td><?php echo round(floatval($result->positiveness5), 4); ?></td>
+                    <td><?php echo round(floatval($result->negativeness5), 4); ?></td>
                 </tr>
             </table>
+            <br/><br/>
             <?php
                 $scores = array($result->score0, $result->score1, $result->score2, $result->score3, $result->score4, $result->score5);
                 $thresholds = array($result->threshold0, $result->threshold1, $result->threshold2, $result->threshold3, $result->threshold4, $result->threshold5);
             ?>
-            <center>
-            <table border = "1" id = "dtable">
-                <tr>
-                    <td><div id="plt_div0" style="width:400px; height:300px;"><?php echo "<script>get_density('plt_div0',0,".$scores[0].",".$thresholds[0].");</script>"; ?></div></td>
-                    <td><div id="plt_div1" style="width:400px; height:300px;"><?php echo "<script>get_density('plt_div1',1,".$scores[1].",".$thresholds[1].");</script>"; ?></div></td>
-                    <td><div id="plt_div2" style="width:400px; height:300px;"><?php echo "<script>get_density('plt_div2',2,".$scores[2].",".$thresholds[2].");</script>"; ?></div></td>
-                </tr>
-                <tr>
-                    <td><div id="plt_div3" style="width:400px; height:300px;"><?php echo "<script>get_density('plt_div3',3,".$scores[3].",".$thresholds[3].");</script>"; ?></div></td>
-                    <td><div id="plt_div4" style="width:400px; height:300px;"><?php echo "<script>get_density('plt_div4',4,".$scores[4].",".$thresholds[4].");</script>"; ?></div></td>
-                    <td><div id="plt_div5" style="width:400px; height:300px;"><?php echo "<script>get_density('plt_div5',5,".$scores[5].",".$thresholds[5].");</script>"; ?></div></td>
-                </tr>
-            </table>
-            </center>
+            <div class="plot_container">
+                <div style="width:49%; float:left;">
+                    <div id="plt_div0" class="plot_div"><?php echo "<script>get_density('plt_div0',0,".$scores[0].",".$thresholds[0].");</script>"; ?></div>
+                    <div class="plot_caption">Classifier 0</div><br/>
+                    <div id="plt_div2" class="plot_div"><?php echo "<script>get_density('plt_div2',2,".$scores[2].",".$thresholds[2].");</script>"; ?></div>
+                    <div class="plot_caption">Classifier 2</div><br/>
+                    <div id="plt_div4" class="plot_div"><?php echo "<script>get_density('plt_div4',4,".$scores[4].",".$thresholds[4].");</script>"; ?></div>
+                    <div class="plot_caption">Classifier 4</div><br/>
+                </div>
+                <div style="width:49%; float:right;">
+                    <div id="plt_div1" class="plot_div"><?php echo "<script>get_density('plt_div1',1,".$scores[1].",".$thresholds[1].");</script>"; ?></div>
+                    <div class="plot_caption">Classifier 1</div><br/>
+                    <div id="plt_div3" class="plot_div"><?php echo "<script>get_density('plt_div3',3,".$scores[3].",".$thresholds[3].");</script>"; ?></div>
+                    <div class="plot_caption">Classifier 3</div><br/>
+                    <div id="plt_div5" class="plot_div"><?php echo "<script>get_density('plt_div5',5,".$scores[5].",".$thresholds[5].");</script>"; ?></div>
+                    <div class="plot_caption">Classifier 5</div><br/>
+                </div>
+            </div>
             <?php
+                    echo "<br/><div>";
                     echo count($out)."<br/>";
                     for($i=0;$i<count($out);++$i)
                         echo $out[$i]."<br/>";
                     echo $status;
+                    echo "</div>";
                 }
             ?>
+            
+            </div>
         </div>
     </body>
 </html> 
