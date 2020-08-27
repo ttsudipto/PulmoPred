6432afc9672cde39f4aa727f701b6e7eb8ece457
Sudipto Bhattacharjee <ttsudipto@gmail.com>
2020-08-27 21:49:23 +0530

    Classes for wrapping output and for encoding as JSON

---
+++ python/output_wrapper.py
@@ -0,0 +1,31 @@
+from json import JSONEncoder
+
+class Output :
+    def __init__(self, classifier):
+        self.classifier = classifier
+        self.version = None
+        self.labels = []
+        self.scores = []
+        self.probas = []
+        
+        self.paths = []
+        
+        self.thresholds = []
+        self.positivenesses = []
+        self.negativenesses = []
+
+class OutputEncoder(JSONEncoder) :
+    def default(self, obj):
+        if isinstance(obj, Output) :
+            out_dict = dict()
+            out_dict['classifier'] = obj.classifier
+            out_dict['version'] = obj.version
+            out_dict['labels'] = obj.labels
+            out_dict['scores'] = obj.scores
+            out_dict['probas'] = obj.probas
+            out_dict['paths'] = obj.paths
+            out_dict['thresholds'] = obj.thresholds
+            out_dict['positivenesses'] = obj.positivenesses
+            out_dict['negativenesses'] = obj.negativenesses
+            return out_dict
+        return JSONEncoder.default(self, obj)
