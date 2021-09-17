11964c0643c6019bf0c9412e1dfb6259bc3fd493
Sudipto Bhattacharjee <ttsudipto@gmail.com>
2021-09-17 16:45:37 +0530

    Modify ROC modules for models with and w/o under-sampling

--- python/ml/roc.py
+++ python/ml/roc.py
@@ -1,17 +1,73 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import copy
+#import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
 from scipy import interp
+from . import reader
+from .model import Model, get_under_sampling_folds
 from .pickler import load_saved_models
 from ..config import ml_config as mlc
 
-def plot_roc_model(model, do_fit=False) :
+us_svm_hyperparameters = mlc.get_optimal_hyperparameters('us', mlc.get_SVM_id())
+us_rf_hyperparameters = mlc.get_optimal_hyperparameters('us', mlc.get_RandomForest_id())
+us_nb_hyperparameters = mlc.get_optimal_hyperparameters('us', mlc.get_NaiveBayes_id())
+us_mlp_hyperparameters = mlc.get_optimal_hyperparameters('us', mlc.get_MLP_id())
+no_us_svm_hyperparameters = mlc.get_optimal_hyperparameters('no-us', mlc.get_SVM_id())
+no_us_rf_hyperparameters = mlc.get_optimal_hyperparameters('no-us', mlc.get_RandomForest_id())
+no_us_nb_hyperparameters = mlc.get_optimal_hyperparameters('no-us', mlc.get_NaiveBayes_id())
+no_us_mlp_hyperparameters = mlc.get_optimal_hyperparameters('no-us', mlc.get_MLP_id())
+
+us_SVM_params = {
+    'C' : us_svm_hyperparameters['C'],
+    'gamma' : us_svm_hyperparameters['gamma']
+    }
+us_SVM_thresholds = us_svm_hyperparameters['thresholds']
+no_us_SVM_params = {
+    'C' : no_us_svm_hyperparameters['C'],
+    'gamma' : no_us_svm_hyperparameters['gamma']
+    }
+no_us_SVM_threshold = no_us_svm_hyperparameters['thresholds'][0]
+us_RF_params = {
+    'n_estimators' : us_rf_hyperparameters['n_estimators'],
+    'max_depth' : us_rf_hyperparameters['max_depth'],
+    'max_features' : us_rf_hyperparameters['max_features']
+    }
+no_us_RF_params = {
+    'n_estimators' : no_us_rf_hyperparameters['n_estimators'],
+    'max_depth' : no_us_rf_hyperparameters['max_depth'],
+    'max_features' : no_us_rf_hyperparameters['max_features']
+    }
+us_GNB_params = {
+    'var_smoothing' : us_nb_hyperparameters['smoothing']
+    }
+no_us_GNB_params = {
+    'var_smoothing' : no_us_nb_hyperparameters['smoothing']
+    }
+us_MLP_params = {
+    'activation' : us_mlp_hyperparameters['activation'],
+    'hidden_layer_sizes' : us_mlp_hyperparameters['hidden_layer_sizes'],
+    'learning_rate_init' : us_mlp_hyperparameters['learning_rate_init']
+    }
+no_us_MLP_params = {
+    'activation' : no_us_mlp_hyperparameters['activation'],
+    'hidden_layer_sizes' : no_us_mlp_hyperparameters['hidden_layer_sizes'],
+    'learning_rate_init' : no_us_mlp_hyperparameters['learning_rate_init']
+    }
+
+res = None
+under_sample_folds = None
+
+def take_input() :
+    global res
+    global under_sample_folds
+    res = reader.read_data('PFT_O_NO_uncommon.csv', verbose=False)
+    under_sample_folds = get_under_sampling_folds(res.target, 1, mlc.get_n_US_folds())
+
+def plot_roc_model(model, verbose=False) :
     tprs = []
     aucs = []
     mean_fpr = np.linspace(0, 1, 100)
 
-    if do_fit == True :
-        model.learn()
     for i in range(len(model.estimators)) :
         X_test = model.data[model.test_indices[i]]
         y_test = model.target[model.test_indices[i]]
@@ -21,6 +77,8 @@ def plot_roc_model(model, do_fit=False) :
         elif mlc.is_RandomForest_id(model.estimator_id) :
             fpr, tpr, thresholds = roc_curve(y_test, probas[:, 1])
         elif mlc.is_NaiveBayes_id(model.estimator_id) :
+            fpr, tpr, thresholds = roc_curve(y_test, probas[:, 1])
+        elif mlc.is_MLP_id(model.estimator_id) :
             fpr, tpr, thresholds = roc_curve(y_test, probas[:, 1])
         else :
             raise ValueError('Invalid estimator ID')
@@ -36,6 +94,9 @@ def plot_roc_model(model, do_fit=False) :
     mean_tpr[-1] = 1.0
     mean_auc = auc(mean_fpr, mean_tpr)
     std_auc = np.std(aucs)
+    
+    if verbose == True :
+        print('\tMean AUC = ' + str(mean_auc) + ' (+/- ' + str(std_auc) + ')')
     
     #------ Print as CSV ------#
     #heading = ['FPR']
@@ -91,6 +152,72 @@ def plot_roc_US(models) :
     
     return mean_fpr, mean_tpr, mean_auc, std_auc
 
-def execute(estimator_id) :
-    models = load_saved_models(estimator_id)
-    plot_roc_US(models)
+def create_model_us(estimator_id) :
+    models = []
+    
+    for i in range(len(under_sample_folds)) :
+        if mlc.is_SVM_id(estimator_id) : ##SVM
+            m = Model(estimator_id, res.data[under_sample_folds[i]], res.target[under_sample_folds[i]])
+            for p, v in us_SVM_params.items() :
+                m.set_estimator_param(p, v)
+        elif mlc.is_RandomForest_id(estimator_id) : ## RF
+            m = Model(estimator_id, res.data[under_sample_folds[i]], res.target[under_sample_folds[i]])
+            for p, v in us_RF_params.items() :
+                m.set_estimator_param(p, v)
+        elif mlc.is_NaiveBayes_id(estimator_id) : ## GNB
+            m = Model(estimator_id, res.data[under_sample_folds[i]], res.target[under_sample_folds[i]])
+            for p, v in us_GNB_params.items() :
+                m.set_estimator_param(p, v)
+        elif mlc.is_MLP_id(estimator_id) : ## MLP
+            m = Model(estimator_id, res.data[under_sample_folds[i]], res.target[under_sample_folds[i]])
+            for p, v in us_MLP_params.items() :
+                m.set_estimator_param(p, v)
+        
+        m.learn()
+        models.append(copy.deepcopy(m))
+    
+    return models
+
+def create_model_no_us(estimator_id) :
+    if mlc.is_SVM_id(estimator_id) :
+        m = Model(estimator_id, res.data, res.target)
+        for p, v in no_us_SVM_params.items() :
+            m.set_estimator_param(p, v)
+    elif mlc.is_RandomForest_id(estimator_id) :
+        m = Model(estimator_id, res.data, res.target)
+        for p, v in no_us_RF_params.items() :
+            m.set_estimator_param(p, v)
+    elif mlc.is_NaiveBayes_id(estimator_id) :
+        m = Model(estimator_id, res.data, res.target)
+        for p, v in no_us_GNB_params.items() :
+            m.set_estimator_param(p, v)
+    elif mlc.is_MLP_id(estimator_id) :
+        m = Model(estimator_id, res.data, res.target)
+        for p, v in no_us_MLP_params.items() :
+            m.set_estimator_param(p, v)
+    
+    m.learn()
+    return m
+
+def execute(estimator_id, dataset) :
+    #models = load_saved_models(estimator_id)
+    #plot_roc_US(models)
+    
+    take_input()
+    
+    if dataset == 'us' :
+        models = create_model_us(estimator_id)
+        plot_roc_US(models)
+    elif dataset == "no-us" :
+        model = create_model_no_us(estimator_id)
+        plot_roc_model(model, verbose=True)
+
+#execute('SVM', 'us')
+#execute('RF', 'us')
+#execute('GNB', 'us')
+#execute('MLP', 'us')
+
+#execute('SVM', 'no-us')
+#execute('RF', 'no-us')
+#execute('GNB', 'no-us')
+#execute('MLP', 'no-us')
